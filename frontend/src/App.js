885039dd79bfad0a45a7443bc3c6b917bf3f3351
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-04-30 21:35:40 +0600

    Update `App.js`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Route } from "react-router-dom";
+
+import homepage from "./pages/homepage";
+import chatpage from "./pages/chatpage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route path="/" component={homepage} exact />
+      <Route path="/chats" component={chatpage} />
+    </div>
+  );
 }
 
 export default App;
