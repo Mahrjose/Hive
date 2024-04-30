40b3e09cef3ab9415b0f54032a2ea09e1b6bf4e3
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-04-30 23:27:18 +0600

    Update deploy settings

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,23 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// -------------------- Deployment -------------------- //
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+// -------------------- Deployment -------------------- //
 
 app.use(notFound);
 app.use(errorHandler);
