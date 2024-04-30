fa9fce7f9f6105628accb9ab6ac0ffb79037917b
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-04-30 23:26:50 +0600

    Update `db.js`

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
-const colors = require('colors');
+const mongoose = require("mongoose");
+const colors = require("colors");
 
-const connectDB = async() => {
+const connectDB = async () => {
   try {
-    const conn =  await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/hive`, {
       // userNewUrlParser: true,
       // userUnifiedTopology: true,
       // userCreateIndex: true,
     });
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
-
   } catch (error) {
     console.log(`Error: ${error.message}`.red.bold);
     process.exit();
-  } 
-}
+  }
+};
 
 module.exports = connectDB;
